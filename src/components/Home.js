91d31ee2f38b29a9d280a0cb6bce444df5455b92
95871abhinandankumar <54628250+95871abhinandankumar@users.noreply.github.com>
2021-07-19 02:16:29 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
 
     return (
         <main className="">
-            <img src={image} alt="Backgroung Image" className="absolute object-cover w-full h-full min-h-screen"/>
+            <img src={image} alt="Backgroung_Image" className="absolute object-cover w-full h-full min-h-screen"/>
             <section className="relative flex justify-between min-h-screen pt-12 lg:pt-32 px-8" >
                 <h1 className="mt-16 text-6xl text-black-600 font-bold cursive leading-none lg:leading-snug  text-left align-bottom hover:text-pink-600" id="changeColor">Hii,<br /> I'm Abhinandan kumar.<br/><h2 id="changeColor1" className="text-8xl">Welcome to my-website.</h2></h1>
                 </section>
